8f0d74e2cc32bc8cc881944cbe0b5ded839f519d
Matt Sicker <matt.sicker@spr.com>
2016-11-22 20:02:37 -0600

    Add CORS policy

--- src/main/java/com/spr/controller/BlogController.java
+++ src/main/java/com/spr/controller/BlogController.java
@@ -7,6 +7,7 @@ import com.spr.service.BlogService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +23,7 @@ import reactor.core.publisher.Mono;
  * @author Matt Sicker
  */
 @RestController
+@CrossOrigin("http://localhost:8000")
 @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class BlogController {
