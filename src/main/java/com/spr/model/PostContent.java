a5a820cfc73aa4c220ba96376188c117272c56b1
Matt Sicker <matt.sicker@spr.com>
2016-11-23 15:00:01 -0600

    Add simple integration tests

--- src/main/java/com/spr/model/PostContent.java
+++ src/main/java/com/spr/model/PostContent.java
@@ -2,8 +2,10 @@ package com.spr.model;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.NonNull;
 import lombok.Value;
+import lombok.experimental.Wither;
 
 /**
  * REST API data format for blog posts.
@@ -11,6 +13,8 @@ import lombok.Value;
  * @author Matt Sicker
  */
 @Value
+@Builder
+@Wither
 @AllArgsConstructor(onConstructor = @__(@JsonCreator))
 public class PostContent {
     @NonNull
--- src/test/java/com/spr/ReactorExampleApplicationTests.java
+++ src/test/java/com/spr/ReactorExampleApplicationTests.java
@@ -1,17 +1,27 @@
 package com.spr;
 
 import java.io.IOException;
+import java.util.UUID;
 
+import com.spr.controller.BlogController;
+import com.spr.model.PostContent;
 import com.spr.setup.CassandraSetup;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import reactor.core.publisher.Mono;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ReactorExampleApplicationTests {
+
+    @Autowired
+    private BlogController controller;
 
     @BeforeClass
     public static void setUpClass() throws IOException {
@@ -19,7 +29,19 @@ public class ReactorExampleApplicationTests {
     }
 
     @Test
-    public void contextLoads() {
+    public void sanityTests() throws Exception {
+        final Mono<PostContent> nonexistent = controller.getPost(UUID.randomUUID());
+        assertThat(nonexistent.hasElement().block()).isFalse();
+        final UUID id = controller.addPost(
+            Mono.just(newPostContent())).block();
+        final Mono<PostContent> contentMono = controller.getPost(id);
+        assertThat(contentMono.block()).isEqualTo(newPostContent());
+        controller.updatePost(id, Mono.just(newPostContent().withBody("Other body"))).block();
+        final Mono<PostContent> updatedMono = controller.getPost(id);
+        assertThat(updatedMono.block()).isEqualTo(newPostContent().withBody("Other body"));
     }
 
+    private static PostContent newPostContent() {
+        return PostContent.builder().title("Title").author("Author").body("Body").build();
+    }
 }
