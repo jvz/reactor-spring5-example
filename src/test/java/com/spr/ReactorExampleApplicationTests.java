c5cba7d7029e167de032a9a29e570778ab88c0ec
Matt Sicker <matt.sicker@spr.com>
2016-11-23 14:46:29 -0600

    Fix broken test

--- src/test/java/com/spr/ReactorExampleApplicationTests.java
+++ src/test/java/com/spr/ReactorExampleApplicationTests.java
@@ -1,16 +1,25 @@
 package com.spr;
 
+import java.io.IOException;
+
+import com.spr.setup.CassandraSetup;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ReactorExampleApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @BeforeClass
+    public static void setUpClass() throws IOException {
+        CassandraSetup.init();
+    }
+
+    @Test
+    public void contextLoads() {
+    }
 
 }
